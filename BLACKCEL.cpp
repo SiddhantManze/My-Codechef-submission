19be9bea77c635dbe426a5d03344c29767e7016a
Siddhant <88947193+SiddhantManze@users.noreply.github.com>
2021-09-05 21:57:37 +0530

    Create BLACKCEL.cpp

---
+++ BLACKCEL.cpp
@@ -0,0 +1,16 @@
+/*
+    PROBLEM: BLACKCEL
+    AUTHOR : SIDDHANT MANZE
+    DATE   : 24/08/2021
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<(n*n)/2<<endl;
+    return 0;
+}
