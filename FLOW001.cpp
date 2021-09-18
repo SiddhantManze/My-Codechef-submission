846c8b5742397dc4b0fba8e0161450c560ce1fc8
Siddhant <88947193+SiddhantManze@users.noreply.github.com>
2021-09-18 17:55:20 +0530

    Create FLOW001.cpp

---
+++ FLOW001.cpp
@@ -0,0 +1,21 @@
+/*
+    PROBLEM : FLOW001
+*/
+
+#include<iostream>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin>>t;
+    
+    while(t--)
+    {
+        ll a,b;
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+    return 0;
+}
