e250dfbdad12b55d889ee565f18ca55e55690452
Siddhant <88947193+SiddhantManze@users.noreply.github.com>
2021-09-20 22:06:22 +0530

    Create FLOW006.cpp

---
+++ FLOW006.cpp
@@ -0,0 +1,25 @@
+/*
+    
+*/
+#include<iostream>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,sum=0;
+        cin>>n;
+        while(n!=0)
+        {
+            ll d=n%10;
+            sum=sum+d;
+            n=n/10;
+        }
+        cout<<sum<<endl;
+    }
+    return 0;
+}
